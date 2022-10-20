6c847c740088b2b635453684f537e3df9c39a518
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-10-21 00:57:12 +0300

    Completed the lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
-class Person
 
-end
+
+class Person
+    attr_accessor :name
+    attr_accessor :job
+    def name
+      @name
+    end
+  
+    def name=(name)
+      @name = name
+    end
+  
+    # def initialize(name,job)
+    #   @name = name  
+    #   @job = job
+    # end
+  
+    def job=(job)
+      @job = job
+    end
+  end
